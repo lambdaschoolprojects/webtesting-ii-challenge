d8269cd246b40dfba7679573c7740919d0559dc8
Rane <rane@blasph.me>
2019-06-21 18:29:06 -0700

    stubbing in components

---
+++ src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Dashboard from './Dashboard';
+import Display from './Display';
+
+const Display = () => {
+    return (
+        <>
+            <Display/>
+            <Dashboard/>
+        </>
+    )
+}
+
+export default Display;
